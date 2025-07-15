6e909b93ddbf1858bac187b0ba45e5d92f7e621e
MishraAbhijit <m.abhijitmishra97@gmail.com>
2025-07-16 00:59:07 +0530

    Refactoring code and enabling swagger for all the services

--- transaction-coordinator/src/main/java/com/abhijit/transactioncoordinator/service/impl/TransactionServiceImpl.java
+++ transaction-coordinator/src/main/java/com/abhijit/transactioncoordinator/service/impl/TransactionServiceImpl.java
@@ -32,22 +32,22 @@ public class TransactionServiceImpl implements TransactionService {
             //Phase 1: Initializing Transaction - Prepare Phase
             ResponseEntity<String> inventoryPrep = restTemplate.postForEntity("http://localhost:8082/inventory/prepare", orderRequest, String.class);
             ResponseEntity<String> paymentPrep = restTemplate.postForEntity("http://localhost:8083/payment/prepare", orderRequest, String.class);
-            ResponseEntity<String> orderPrep = restTemplate.postForEntity("http://localhost:8081/order/prepare", orderRequest, String.class);
+            ResponseEntity<String> orderPrep = restTemplate.postForEntity("http://localhost:8084/order/prepare", orderRequest, String.class);
 
             if (inventoryPrep.getStatusCode().is2xxSuccessful() && paymentPrep.getStatusCode().is2xxSuccessful() && orderPrep.getStatusCode().is2xxSuccessful()) {
                 // Phase 2: Commit Phase
-                restTemplate.postForEntity("http://localhost:8081/order/commit", orderRequest, String.class);
                 restTemplate.postForEntity("http://localhost:8082/inventory/commit", orderRequest, String.class);
                 restTemplate.postForEntity("http://localhost:8083/payment/commit", orderRequest, String.class);
+                restTemplate.postForEntity("http://localhost:8084/order/commit", orderRequest, String.class);
                 return ResponseEntity.ok("Transaction committed successfully");
             } else {
                 throw new IllegalStateException("Prepare phase failed... ");
             }
         } catch (Exception exception) {
             // Phase 3: Rollback Phase
-            restTemplate.postForEntity("http://localhost:8081/order/rollback", orderRequest, String.class);
             restTemplate.postForEntity("http://localhost:8082/inventory/rollback", orderRequest, String.class);
             restTemplate.postForEntity("http://localhost:8083/payment/rollback", orderRequest, String.class);
+            restTemplate.postForEntity("http://localhost:8084/order/rollback", orderRequest, String.class);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Transaction rolled back due to failure");
         }
     }
